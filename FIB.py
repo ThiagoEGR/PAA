6d6bc8acc5ef43327ee8f6511a65fd929999581e
ThiagoEGR <tegr@ic.ufal.br>
2021-03-30 01:19:29 -0300

    Update FIB.py

--- FIB.py
+++ FIB.py
@@ -1,7 +1,7 @@
 from timeit import timeit
 from matplotlib import pyplot as plt
 
-
+#fibonacci recursivo
 def fibRec(n):
     if n < 2:
         return n
@@ -9,6 +9,7 @@ def fibRec(n):
     else:
         return fibRec(n - 1) + fibRec (n - 2)
 
+#fibonacci iterativo 
 def fibIte(n):
     j = 1
     i = 0
@@ -22,11 +23,14 @@ def fibIte(n):
 
 
 
-    
+#criando uma lista de 1 a 41    
 n = [ i for i in range(1,42,1)]
+#calcula o tempo de fibonacci recursivo do número 1 a 41 e add a lista y1
 y1 = [timeit(lambda: fibRec(i), number = 1)for i in range(1, 42, 1)]
+#calcula o tempo de fibonacci iterativo do número 1 a 41 e add a lista y2
 y2 = [timeit(lambda: fibIte(i), number = 1)for i in range(1, 42, 1)]
 
+#imprime o gráfico
 plt.xlabel('n')
 plt.ylabel('segundos')
 plt.plot(n, y1,  'go-')
